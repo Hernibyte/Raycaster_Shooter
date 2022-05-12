9663651d5dafa50ce22a32aad0ac5321012a1bd4
Gossa Hernan <hernanagustin.g24@hotmail.com>
2022-05-12 20:34:34 -0300

    Added a abasic player

--- RaycasterShooter/src/Source.cpp
+++ RaycasterShooter/src/Source.cpp
@@ -1,18 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "freeglut.h"
 
-#define WIDTH 800
-#define HEIGHT 800
+#define WIDTH 1024
+#define HEIGHT 512
+
+float player_x, player_y; // player position
+
+void drawPlayer() {
+	glColor3f(1, 1, 0);
+	glPointSize(8);
+	glBegin(GL_POINTS);
+	glVertex2i(player_x, player_y);
+	glEnd();
+}
 
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	drawPlayer();
+
 	glutSwapBuffers();
+}
+
+void init() {
+	glClearColor(0.3, 0.3, 0.3, 0);
+	gluOrtho2D(0, 1024, 512, 0);
+
+	player_x = 300, player_y = 300;
 }
 
 int main(int argc, char* argv[]) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowSize(WIDTH, HEIGHT);
-	glutCreateWindow("");
+	glutCreateWindow("Raycaster Shooter");
+	init();
 	glutDisplayFunc(display);
 	glutMainLoop();
 }
